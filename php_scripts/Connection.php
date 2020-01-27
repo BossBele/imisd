2c6e6303f898b906326d487ab85fbbc8a11bca2a
maricajr <maricaneston38@gmail.com>
2020-01-27 13:16:45 +0300

    delete added

--- php_scripts/Connection.php
+++ php_scripts/Connection.php
@@ -12,32 +12,11 @@ class Connection
         /*create connection*/
         $conn = new mysqli($servername, $username, $conn_password, $dbname);
 
-//$hashed_password = password_hash("12345678", PASSWORD_DEFAULT);
-//var_dump($hashed_password);
-
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
         return $conn;
-        /*close connection*/
-//$conn->close();
     }
 }
 
-//$servername = "localhost";
-//$username = "root";
-//$conn_password = "";
-//$dbname = "imi";
-//
-///*create connection*/
-//$conn = new mysqli($servername, $username, $conn_password,$dbname);
-//
-////$hashed_password = password_hash("12345678", PASSWORD_DEFAULT);
-////var_dump($hashed_password);
-//
-//if ($conn->connect_error) {
-//    die("Connection failed: " . $conn->connect_error);
-//}
-///*close connection*/
-////$conn->close();
